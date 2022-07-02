57b435defaa0b624b715b5de80c36bdad9768fb4
Diego Santos <diegoleonardo@outlook.com>
2022-07-02 13:43:56 -0300

    chore: integrate the account domain to use case

--- src/application/account/create.clj
+++ src/application/account/create.clj
@@ -1,9 +1,10 @@
 (ns application.account.create
   (:require [domain.validator :as validator]
-            [domain.schema.account :as schema]))
+            [domain.schema.account :as schema]
+            [domain.account :as account]))
 
 (defn execute! [account-input]
   (if-let [error (validator/humanized-error schema/create account-input)]
     error
     {:success true
-     :data    account-input}))
+     :data    (account/create account-input)}))
