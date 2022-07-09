2fe1dc7e2520826160b4566303178b35819db65a
Diego Santos <diegoleonardo@outlook.com>
2022-07-09 16:18:09 -0300

    chore: created the http router component and the implementation adapter as well

---
+++ test/infra/api/rest/reitit_adapter_test.clj
@@ -0,0 +1,19 @@
+(ns infra.api.rest.reitit-adapter-test
+  (:require [infra.api.http-router :as router]
+            [infra.api.rest.reitit-adapter :as adapter]
+            [clojure.test :refer [deftest testing is]]
+            [matcher-combinators.test :refer [match?]]))
+
+(def route-example [["/test"
+                     {:get
+                      {:handler (fn [_]
+                                  {:status 200
+                                   :body   "hello world"})}}]])
+
+(deftest create-route
+  (testing "should be possible creating a route"
+    (let [route (router/create (adapter/->reitit-adapter route-example))]
+      (is (match? {:status 200
+                   :body   "hello world"}
+                  (route {:request-method :get
+                          :uri            "/v1/test"}))))))
