11c99e7cb218839ba67925d79f291f7057aab550
Diego Santos <diegoleonardo@outlook.com>
2022-07-16 00:59:29 -0300

    refac: refactored the account route test

--- test/infra/api/rest/routes/account_test.clj
+++ test/infra/api/rest/routes/account_test.clj
@@ -7,23 +7,23 @@
             [muuntaja.core :as m]
             [utils.account :as account-utils]))
 
-(deftest route
-  (testing "should create a route"
-    (testing "to create an account"
-      (let [route    (-> (account-utils/deps)
-                         account/route
-                         adapter/->reitit-adapter
-                         router/create)
-            response (route {:request-method :post
-                             :uri            "/v1/account"
-                             :headers        {"content-type" "application/edn"
-                                              "accept"       "application/transit+json"}
-                             :body-params    account-utils/account-example})]
-        (is (match? {:status 200} response))
-        (is (match? {:account-id string?}
-                    (m/decode-response-body response)))))
+(deftest create-account
+  (testing "should create a route to create an account"
+    (let [route    (-> (account-utils/deps)
+                       account/route
+                       adapter/->reitit-adapter
+                       router/create)
+          response (route {:request-method :post
+                           :uri            "/v1/account"
+                           :headers        {"content-type" "application/edn"
+                                            "accept"       "application/transit+json"}
+                           :body-params    account-utils/account-example})]
+      (is (match? {:status 200} response))
+      (is (match? {:account-id string?}
+                  (m/decode-response-body response))))))
 
-    (testing "to fetch an account"
+(deftest fetch
+  (testing "should create a route to fetch an account"
       (testing "found"
         (let [route    (-> (account-utils/deps account-utils/account-data-store-example)
                            account/route
@@ -50,18 +50,19 @@
           response
           (is (match? {:status 404} response))
           (is (match? {}
-                      (m/decode-response-body response))))))
+                      (m/decode-response-body response)))))))
 
-    (testing "to patch an account"
-      (let [route    (-> (account-utils/deps account-utils/account-data-store-example)
-                         account/route
-                         adapter/->reitit-adapter
-                         router/create)
-            response (route {:request-method :patch
-                             :uri            (str "/v1/account/" account-utils/reference-id)
-                             :headers        {"content-type" "application/edn"
-                                              "accept"       "application/transit+json"}
-                             :body-params    account-utils/account-example})]
-        (is (match? {:status 200} response))
-        (is (match? account-utils/expected-response
-                    (m/decode-response-body response)))))))
+(deftest patch
+  (testing "should create a route to patch an account"
+    (let [route    (-> (account-utils/deps account-utils/account-data-store-example)
+                       account/route
+                       adapter/->reitit-adapter
+                       router/create)
+          response (route {:request-method :patch
+                           :uri            (str "/v1/account/" account-utils/reference-id)
+                           :headers        {"content-type" "application/edn"
+                                            "accept"       "application/transit+json"}
+                           :body-params    account-utils/account-example})]
+      (is (match? {:status 200} response))
+      (is (match? account-utils/expected-response
+                  (m/decode-response-body response))))))
