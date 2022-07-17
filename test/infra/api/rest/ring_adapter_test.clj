6046ef4b68a2161fa78eb4ea5c1d55f05d8ecc62
Diego Santos <diegoleonardo@outlook.com>
2022-07-16 23:06:19 -0300

    refac: changed the http client lib and added the utils to use this library

--- test/infra/api/rest/ring_adapter_test.clj
+++ test/infra/api/rest/ring_adapter_test.clj
@@ -1,12 +1,9 @@
 (ns infra.api.rest.ring-adapter-test
   (:require [infra.api.http-server :as http-server]
             [infra.api.rest.ring-adapter :as ring-adapter]
-            [org.httpkit.client :as http-client]
+            [utils.http-utils :as http-utils]
             [clojure.test :refer [deftest testing is]]
             [matcher-combinators.test :refer [match?]]))
-
-(defn- request [url]
-  @(http-client/get url))
 
 (defn- handler [_]
   {:status  200
@@ -15,11 +12,11 @@
 
 (deftest ^:integration listen
   (testing "should startup a http server"
-    (let [config   {:port   3000}
+    (let [config   {:port 3000}
           adapter  (ring-adapter/->ring-adapter handler)
           endpoint (str "http://localhost:" (:port config))
           server   (http-server/listen adapter config)]
       (is (match? {:status 200
                    :body   "Hello world"}
-                  (request endpoint)))
+                  (http-utils/get-request endpoint)))
       (.stop server))))
